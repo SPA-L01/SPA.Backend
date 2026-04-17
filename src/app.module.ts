3f82f6617f5e2203ffd6b849e4f707f09da444fc
sea835 <148954003+sea835@users.noreply.github.com>
2026-04-17 11:26:32 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getDatabaseConfig } from './config/database.config';
